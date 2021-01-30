6ed025e6eb149986300b1e2017da3eb470b45852
IslamReda <asoeslam@gmail.com>
2021-01-30 06:47:49 +0200

    change profile picture added

--- Client/src/main/java/controllers/ProfileController.java
+++ Client/src/main/java/controllers/ProfileController.java
@@ -13,8 +13,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import dbconnection.PlayerConnection;
+import java.io.File;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -27,6 +31,7 @@ import javafx.scene.input.*;
 import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 import javafx.event.EventHandler;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 /**
@@ -34,7 +39,7 @@ import javafx.scene.image.ImageView;
  * @author Islam
  */
 public class ProfileController implements Initializable {
-    
+
     private Label label;
     @FXML
     private JFXTextField tfName;
@@ -53,29 +58,30 @@ public class ProfileController implements Initializable {
     private boolean isFromPlayingMode;
     private PlayerConnection connectPlayer;
     private Player player;
+    private String imgProfilePic;
 
-     public void getData(Player player,boolean flag,PlayerConnection connectPlayer)
-    {
+    public void getData(Player player, boolean flag, PlayerConnection connectPlayer) {
         this.connectPlayer = connectPlayer;
         this.player = player;
         isFromPlayingMode = flag;
         tfName.setText(player.getName());
         tfEmail.setText(player.getEmail());
-        tfPassword.setText(player.getPassword());       
+        tfPassword.setText(player.getPassword());
         tfScore.setText(Integer.toString(player.getMain_score()));
+        imgProfile.setImage(new Image(getClass().getResourceAsStream("/icons/" + player.getAvatar())));
+        imgProfilePic = player.getAvatar();
     }
-    
-     @FXML
-    private void logoutButtonPushed(ActionEvent event)  throws IOException
-    {
+
+    @FXML
+    private void logoutButtonPushed(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/fxmls/Home.fxml"));
         Scene scene = new Scene(root);
 
         //This line gets the Stage information
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         window.setScene(scene);
-        connectPlayer.serialaize("logout",player);
+        connectPlayer.serialaize("logout", player);
         Map<String, Player> elements = connectPlayer.deserialize();
         System.out.println(elements.values().toArray()[0]);
 
@@ -83,10 +89,10 @@ public class ProfileController implements Initializable {
         System.out.println("closed");
         window.show();
     }
-     @FXML
-    private void backButtonPushed(ActionEvent event)  throws IOException
-    {
-        if(isFromPlayingMode){
+
+    @FXML
+    private void backButtonPushed(ActionEvent event) throws IOException {
+        if (isFromPlayingMode) {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/fxmls/MatchGround.fxml"));
             Parent root = loader.load();
@@ -95,16 +101,15 @@ public class ProfileController implements Initializable {
 
             //access the controller and call a method
             MatchGroundController controller = loader.getController();
-           controller.init(player,connectPlayer);
+            controller.init(player, connectPlayer);
 
             //This line gets the Stage information
-            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
             window.setScene(scene);
             window.show();
-        }
-        else{
-             FXMLLoader loader = new FXMLLoader();
+        } else {
+            FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/fxmls/PlayingMode.fxml"));
             Parent root = loader.load();
 
@@ -112,55 +117,101 @@ public class ProfileController implements Initializable {
 
             //access the controller and call a method
             PlayingModeController controller = loader.getController();
-           controller.init(player,connectPlayer);
+            controller.init(player, connectPlayer);
 
             //This line gets the Stage information
-            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
             window.setScene(scene);
             window.show();
         }
     }
-     @FXML
-    private void saveButtonPushed(ActionEvent event)  throws IOException
-    {
-        if(!"".equals(tfName.getText())){
-            if(!"".equals(tfPassword.getText())){
+
+    @FXML
+    private void saveButtonPushed(ActionEvent event) throws IOException {
+        if (!"".equals(tfName.getText())) {
+            if (!"".equals(tfPassword.getText())) {
                 player = new Player(tfName.getText(), tfEmail.getText(), tfPassword.getText());
-                connectPlayer.serialaize("updateProfile",player);
+                player.setAvatar(imgProfilePic);
+                connectPlayer.serialaize("updateProfile", player);
 
                 Map<String, Player> elements = connectPlayer.deserialize();
-                System.out.println("bol here :"+elements.keySet().toArray()[0].equals("true"));
-                
+                System.out.println("bol here :" + elements.keySet().toArray()[0].equals("true"));
+
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Success");
                 alert.setContentText("Data updated successfully");
                 alert.showAndWait();
-            }else{
-                alertError("Password Wrong","Password must not be empty!");
+            } else {
+                alertError("Password Wrong", "Password must not be empty!");
             }
-        }else{
-            alertError("Username Wrong","Username must not be empty!");
+        } else {
+            alertError("Username Wrong", "Username must not be empty!");
         }
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Success");
         alert.setContentText("Data updated successfully");
         alert.showAndWait();
     }
-    
-    private void alertError(String title , String msg){
-        Alert alert ;
+
+    @FXML
+    private void changeProfilePicture(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxmls/changeProfilePicture.fxml"));
+        Scene scene = new Scene(root);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    private void alertError(String title, String msg) {
+        Alert alert;
         alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
         alert.setContentText(msg);
         //notSavedAlert.showAndWait();
         alert.showAndWait();
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    
-    }    
-    
+        imgProfile.setPickOnBounds(true);
+
+        imgProfile.setOnMouseClicked(new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                try {
+                    FXMLLoader loader = new FXMLLoader();
+                    loader.setLocation(getClass().getResource("/fxmls/changeProfilePicture.fxml"));
+                    Parent root = loader.load();
+                    
+                    Scene scene = new Scene(root);
+                    //This line gets the Stage information
+                    ChangeProfilePictureController controller = loader.getController();
+                    controller.init(player,connectPlayer);
+                    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    
+                    window.setScene(scene);
+                    window.show();
+                } catch (IOException ex) {
+                    Logger.getLogger(ProfileController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+    }
+
+    public void init(Player player , PlayerConnection connectPlayer) {
+        this.connectPlayer = connectPlayer;
+        this.player = player;
+        tfName.setText(player.getName());
+        tfEmail.setText(player.getEmail());
+        tfPassword.setText(player.getPassword());
+        tfScore.setText(Integer.toString(player.getMain_score()));
+        imgProfilePic = player.getAvatar();
+        imgProfile.setImage(new Image(getClass().getResourceAsStream("/icons/" + imgProfilePic)));
+    }
+
+
 }
